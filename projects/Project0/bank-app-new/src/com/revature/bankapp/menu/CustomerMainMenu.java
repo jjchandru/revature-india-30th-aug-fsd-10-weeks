602662289886ec45c76236a02ffd54eda44a9f77
Chandra <jjchandru@yahoo.com>
2021-09-09 12:29:05 +0530

    bank app menu skeleton

--- projects/Project0/bank-app-new/src/com/revature/bankapp/menu/CustomerMainMenu.java
+++ projects/Project0/bank-app-new/src/com/revature/bankapp/menu/CustomerMainMenu.java
@@ -4,6 +4,14 @@ public class CustomerMainMenu extends Menu {
 
 	public CustomerMainMenu(String name) {
 		super(name);
+		addMenuItem("View Accounts");
+		addMenuItem("Create Account");
+	}
+
+	@Override
+	void handleAction() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- projects/Project0/bank-app-new/src/com/revature/bankapp/menu/MainMenu.java
+++ projects/Project0/bank-app-new/src/com/revature/bankapp/menu/MainMenu.java
@@ -1,5 +1,11 @@
 package com.revature.bankapp.menu;
 
+import java.util.Scanner;
+
+import com.revature.bankapp.form.LoginForm;
+import com.revature.bankapp.model.Customer;
+import com.revature.bankapp.model.DataManager;
+
 public class MainMenu extends Menu {
 
 	public MainMenu(String name) {
@@ -12,8 +18,33 @@ public class MainMenu extends Menu {
 
 	@Override
 	void handleAction() {
-		System.out.println("Selected option is: " + selection);
-	}
+		Scanner scanner = new Scanner(System.in);
+		switch (selection) {
+		case 1:
+			System.out.println("\n===");
+			System.out.println("Register new customer");
+			System.out.println("====\n");
+			
+			System.out.print("First Name: ");
+			String firstName = scanner.nextLine();
+			
+			System.out.print("Last Name: ");
+			String lastName = scanner.nextLine();
 
-	
+			System.out.print("Email: ");
+			String email = scanner.nextLine();
+
+			System.out.print("Password: ");
+			String password = scanner.nextLine();
+			
+			DataManager.addCustomer(new Customer(firstName, lastName, email, password));
+			System.out.println("Customer added successfully.");
+			displayMenuAndCaptureSelection();
+			break;
+		case 2:
+			LoginForm loginForm = new LoginForm("Login Form");
+			loginForm.captureDataAndPerformAction();
+			break;
+		}
+	}
 }
